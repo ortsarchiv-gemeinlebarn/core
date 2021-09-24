71a5caf77ff435cb2cb49f7aefb4cf962ddefc25
Jakob Vesely <jakob@appartig.at>
2021-09-24 15:55:32 +0200

    Add First Values and Tests

---
+++ src/oag.Core/Values/ColorSchemes.cs
@@ -0,0 +1,9 @@
+﻿namespace oag.Core.Values
+{
+    public static class ColorSchemes
+    {
+        public const string FullColor = "full";
+        public const string Greyscale = "greyscale";
+        public const string BlackWhite = "blackwhite";
+    }
+}
---
+++ test/oag.Core.Test/Values/VersionTypeTest.cs
@@ -0,0 +1,26 @@
+using oag.Core.Values;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace oag.Core.Test
+{
+    public class VersionTypeTest
+    {
+        [Fact]
+        public void Should_BeUniqueValues_When_CheckAllVersionTypes()
+        {
+            // Arrange
+            var values = typeof(VersionType)
+                .GetFields(BindingFlags.Static | BindingFlags.Public)
+                .Select(f => f.GetValue(null).ToString())
+                .ToList();
+
+            // Act
+            bool unique = values.Distinct().Count() == values.Count();
+
+            // Assert
+            Assert.True(unique);
+        }
+    }
+}
