46a9b977978b5a245edb45d5759dad5c12456159
Jakob Vesely <jakob@appartig.at>
2021-09-26 20:50:06 +0200

    Data Project with Context and First Repository

---
+++ src/oag.Core.Data/Contexts/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using oag.Core.Data.Interfaces;
+using oag.Core.Data.Models;
+
+namespace oag.Core.Data.Contexts
+{
+    public class DataContext : DbContext, IDataContext
+    {
+        public DataContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Fonds> Fonds { get; set; }
+    }
+}
---
+++ src/oag.Core.Data/Repositories/Repository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using oag.Core.Data.Contexts;
+using oag.Core.Data.Interfaces;
+
+namespace oag.Core.Data.Repositories
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        private DataContext dataContext;
+
+        protected Repository(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public T Create(T entity)
+        {
+            dataContext.Add(entity);
+            return entity;
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return dataContext.Set<T>().AsNoTracking();
+        }
+
+        public IQueryable<T> ReadByCondition(Expression<Func<T, bool>> expression)
+        {
+            return dataContext.Set<T>().Where(expression).AsNoTracking();
+        }
+
+        public T Update(T entity)
+        {
+            dataContext.Update(entity);
+            return entity;
+        }
+
+        public T Delete(T entity)
+        {
+            dataContext.Remove(entity);
+            return entity;
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await dataContext.SaveChangesAsync();
+        }
+    }
+}
