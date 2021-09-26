3d98488effb040c87fa2844ba44ea397bd4051ab
Jakob Vesely <jakob@appartig.at>
2021-09-26 20:51:12 +0200

    Add Db Setup in Web Project

--- src/oag.Core.Web/Startup.cs
+++ src/oag.Core.Web/Startup.cs
@@ -2,15 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using oag.Core.Data.Contexts;
 
 namespace oag.Core.Web
 {
@@ -20,12 +18,15 @@ namespace oag.Core.Web
         {
             Configuration = configuration;
         }
-
+        
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(o => o.UseMySql(
+                Configuration["DatabaseConnections:MainDB"], 
+                MySqlServerVersion.LatestSupportedServerVersion));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
